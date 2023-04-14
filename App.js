40b2a2051ff4d462e367da5a3753af6081666dae
Sultan Sadykov <sultansadykov35@gmail.com>
2023-04-14 14:13:18 +0600

    check Sultan git

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text></Text>
+      <Text>Sultan</Text>
       <StatusBar style="auto" />
     </View>
   );
