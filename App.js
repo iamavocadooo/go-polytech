988ee72eb1fbc9be4279996431b070aeba9a690b
Sultan Sadykov <sultansadykov35@gmail.com>
2023-04-14 14:27:47 +0600

    changed main

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text></Text>
+      <Text>Main</Text>
       <StatusBar style="auto" />
     </View>
   );
